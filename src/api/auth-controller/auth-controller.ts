a42c67e082e9ca669b11855251c854d940d75ef0
rouuhehe <yaritza.lopez@utec.edu.pe>
2025-11-16 18:00:59 -0500

    minor changes

--- src/api/auth-controller/auth-controller.ts
+++ src/api/auth-controller/auth-controller.ts
@@ -10,8 +10,7 @@ export interface LoginResponse {
     session_id: string;
 }
 
-//const BASE_URL = import.meta.env.VITE_API_URL;
-const BASE_URL = "https://fi7faricwd.execute-api.us-east-1.amazonaws.com"
+const BASE_URL = import.meta.env.VITE_API_URL;
 export function login(req: LoginRequest) {
     return ResultAsync.fromPromise(
         fetch(`${BASE_URL}/auth/login`, {
