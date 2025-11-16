f9bfa96398a3c47edd69d44a42e9c7aa69634c6f
Ela <miruxchiru@gmail.com>
2025-11-16 11:46:14 -0500

    fix(NavBar)

--- src/common/components/Navbar.tsx
+++ src/common/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import alert from "../../../public/assets/alert.png"
+import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
 function Navbar() {
@@ -14,8 +15,7 @@ function Navbar() {
     return(
         <nav className="w-full flex items-center justify-between bg-white text-white p-1 border-b-2 border-blue-200 font-mono">
 
-            <div className="flex items-center gap-2 px-4 py-2 text-black">
-
+            <Link to="/home" className="flex items-center gap-2 px-4 py-2 text-black hover:opacity-80 transition-opacity">
                 <div className="w-8 h-8 rounded flex items-center justify-center overflow-hidden">
                     <img
                         src={alert}
@@ -27,7 +27,7 @@ function Navbar() {
                 <h1 className="text-xl font-bold tracking-wider drop-shadow-md">
                     ALERTA<span className="text-sky-400">UTEC</span>
                 </h1>
-            </div>
+            </Link>
 
             <div className="flex text-black items-center gap-2 px-4 py-2">
                 <span className="text-sm">{time}</span>
