eed61c20419569623a8dd8d475b9b8cffee6a863
Ela <miruxchiru@gmail.com>
2025-11-15 22:46:38 -0500

    minor changes

--- src/common/pages/HomePage.tsx
+++ src/common/pages/HomePage.tsx
@@ -1,8 +1,14 @@
 function HomePage() {
-    return(
+    return (
         <>
+            <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
+                Bienvenidx a mi app ✨
+            </h1>
+            <p style={{ textAlign: "center", opacity: 0.8 }}>
+                Aquí no hay mucho todavía… pero se siente la vibra de grandeza 👀💫
+            </p>
         </>
-    )
+    );
 }
 
 export default HomePage;
