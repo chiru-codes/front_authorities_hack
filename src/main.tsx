16ae5c4097590ed2909ebd5c3f51ee703303a843
Ela <miruxchiru@gmail.com>
2025-11-15 23:14:21 -0500

    front avances

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./common/router/Routes.tsx";
 
+import './index.css'
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
